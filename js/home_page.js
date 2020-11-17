8ce59277f7c726486148194c75f13e9a6b254016
bab21 <2016061@iiitdmj.ac.in>
2020-11-17 20:00:27 +0530

    getting employee payroll data from local storage

--- js/home_page.js
+++ js/home_page.js
@@ -1,19 +1,33 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event)=>{
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
+
+const getEmployeePayrollDataFromStorage = ()=> {
+    return localStorage.getItem('EmployeePayrollList')?
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')):[];
+
+}
 
 const createInnerHtml= () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                      "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    if(empPayrollList.length==0) return;
+
     let innerHtml=`${headerHtml}`;
-    let empPayrollList = createEmployeePayrollJSON();
+    
+    // let empPayrollList = createEmployeePayrollJSON();
     for(const empPayrollData of empPayrollList){
         innerHtml = `${innerHtml}
             <tr>
                 <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
                 <td>${empPayrollData._name}</td>
                 <td>${empPayrollData._gender}</td>
-                <td>${getDeptHtml(empPayrollData._department)}</td>
+                <td>${getDeptHtml(empPayrollData._departments)}</td>
                 <td>${empPayrollData._salary}</td>
                 <td>${empPayrollData._startDate}</td>
                 <td>
