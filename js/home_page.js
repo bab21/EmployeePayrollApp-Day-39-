5a01ada0bb6578f06d0bc365fb3d60ecd24a56ed
bab21 <2016061@iiitdmj.ac.in>
2020-11-17 19:43:06 +0530

    setting employee payroll details from json object

--- js/home_page.js
+++ js/home_page.js
@@ -5,23 +5,67 @@ window.addEventListener('DOMContentLoaded', (event)=>{
 const createInnerHtml= () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                      "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-
-    const innerHtml = `${headerHtml}
-        <tr>
-                <td><img class="profile" src="../assets/profile-images/Ellipse -2.png" alt=""></td>
-                <td>Sagar Yadav</td>
-                <td>Male</td>
-                <td><div class='dept-label'>HR</div>
-                    <div class='dept-label'>Finance</div></td>
-                <td>3000000</td>
-                <td>1 Nov 2020</td>
+    let innerHtml=`${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for(const empPayrollData of empPayrollList){
+        innerHtml = `${innerHtml}
+            <tr>
+                <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
+                <td>${empPayrollData._name}</td>
+                <td>${empPayrollData._gender}</td>
+                <td>${getDeptHtml(empPayrollData._department)}</td>
+                <td>${empPayrollData._salary}</td>
+                <td>${empPayrollData._startDate}</td>
                 <td>
-                    <img id="1" onclick="remove(this)" alt="delete"
+                    <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete"
                             src="../assets/icons/delete-black-18dp.svg">
-                    <img id="1" alt="edit" onclick="update(this)"
+                    <img name="${empPayrollData._id}" alt="edit" onclick="update(this)"
                             src="../assets/icons/create-black-18dp.svg">        
                 </td>
-        </tr>
-    `;
+            </tr>
+            `;
+    }
     document.querySelector('#table-display').innerHTML=innerHtml;
-};
+}
+
+const getDeptHtml = (deptList) =>{
+    let deptHtml ='';
+    for(const dept of deptList){
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
+    }
+    return deptHtml;
+}
+
+const createEmployeePayrollJSON = ()=>{
+    let empPayrollListLocal =[
+        {
+            _name:'Sagar Yadav',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '500000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -2.png'
+
+        },
+        {
+            _name:'Nancy Yadav',
+            _gender: 'female',
+            _department: [
+                'Engineering',
+                'Sales'
+            ],
+            _salary: '400000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime()+1,
+            _profilePic: '../assets/profile-images/Ellipse -1.png'
+
+        }
+    ];
+    return empPayrollListLocal;
+}
